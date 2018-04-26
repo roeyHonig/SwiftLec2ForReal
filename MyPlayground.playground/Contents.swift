87eea7d730b59241861f69724a0b29bbba0c1b67
roeyHonig <roeyhonig94@gmail.com>
2018-04-26 11:36:55 +0300

    commit1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,8 +4,26 @@ import UIKit
 //Library for UI controls (similiar to Views in Android), Animations, widgets and more
 // UI Kit also imports a framwork (library) named Foundation: Defines the basic types like Int and so on
 
+var optioanlString: String? = "Hello"
+optioanlString = nil //
+
+// This will not compile because xcode try to prevent us a nill pointer exception
+//let c = optioanlString.characters.count
 
 
+// let's imagine we wrote some code that did return something
+// but this code could also return nil
+var optinalUserName:String? = "John Appleseed"
 
+// Optional binding: (if let...! new syntax<--------!)
+// 0). define a new variable cale userName
+// 1). if optianlUserName != nil {define a neew variable:String called userName and not optinal}
+// in convientional sytntx it would look like
+// if optinalUserName != nil {
+//     let userName = optinalUserName
+// }
 
-
+// in swift syntax it looks like
+if let userName = optinalUserName {
+    print(userName)
+}
